f0dbe08c19331ebc2aa2539811aba6267d96bfca
mszkudla <259248@student.pwr.edu.pl>
2023-05-23 23:46:43 +0200

    change image detection algorithm

--- cardRecognition/image_detection.py
+++ cardRecognition/image_detection.py
@@ -1,36 +1,51 @@
 import cv2
-import imutils
+import os
 
-# Load image, grayscale, Gaussian blur, Otsu's threshold, dilate
-def cards_from_image(path_to_image):
-    img = cv2.imread(path_to_image)
-    image = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    original = image.copy()
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-    dilate = cv2.dilate(thresh, kernel, iterations=1)
 
-    # Find contours, obtain bounding box coordinates, and extract ROI
-    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    cnts = sorted(cnts, key=lambda c: cv2.boundingRect(c)[0])
-    #cnts = sorted(cnts, key = cv2.contourArea)
-    image_number = 0
-    for c in cnts:
-        if cv2.contourArea(c) < 0:
-            pause
-        x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 2)
-        ROI = original[y:y + h, x:x + w]
-        if (w > 680 and h > 800):
-            cv2.imwrite("detectedCards/{}.png".format(image_number), ROI)
-            image_number += 1
+def delete_files_in_directory(directory):
+    file_list = os.listdir(directory)
+    for file_name in file_list:
+        file_path = os.path.join(directory, file_name)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
-# cv2.imshow('image', image)
-# cv2.imshow('thresh', thresh)
-# cv2.imshow('dilate', dilate)
-# cv2.waitKey()
 
-cards_from_image("images/test.png")
+delete_files_in_directory('detectedCards')
+
+cards_to_detect = int(input("Enter the number of cards to scan: "))
+
+
+# Load the input image
+image = cv2.imread("images/1.jpg")
+
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Apply edge detection to detect the contours of the tarot cards
+edges = cv2.Canny(gray, 50, 150)
+
+# Find contours in the edge image
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Sort the contours based on their areas in descending order
+contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+# Extract the three contours with the largest areas
+top_contours = contours[:cards_to_detect]
+
+# Extract the tarot card images from the original image using the contours
+extracted_images = []
+for i, contour in enumerate(top_contours):
+    x, y, w, h = cv2.boundingRect(contour)
+    tarot_card_image = image[y:y+h, x:x+w]
+
+    # Check dimensions and rotate if necessary
+    if h < w:
+        rotated_image = cv2.rotate(tarot_card_image, cv2.ROTATE_90_CLOCKWISE)
+        extracted_images.append(rotated_image)
+        # Save the rotated tarot card image
+        cv2.imwrite(f"detectedCards/tarot_card_{i}.jpg", rotated_image)
+    else:
+        extracted_images.append(tarot_card_image)
+        # Save the tarot card image as is
+        cv2.imwrite(f"detectedCards/tarot_card_{i}.jpg", tarot_card_image)
--- cardRecognition/makePrediction.py
+++ cardRecognition/makePrediction.py
@@ -2,46 +2,47 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 
+
 def makePrediction(device,
-                        model: torch.nn.Module, 
-                        image_path: str, 
-                        class_names: list[str] = None, 
-                        transform=None):
+                   model: torch.nn.Module,
+                   image_path: str,
+                   class_names: list[str] = None,
+                   transform=None):
     """Makes a prediction on a target image and plots the image with its prediction."""
-    
+
     # 1. Load in image and convert the tensor values to float32
     target_image = torchvision.io.read_image(str(image_path)).type(torch.float32)
-    
+
     # 2. Divide the image pixel values by 255 to get them between [0, 1]
-    target_image = target_image / 255. 
-    
+    target_image = target_image / 255.
+
     # 3. Transform if necessary
     if transform:
         target_image = transform(target_image)
-    
+
     # 4. Make sure the model is on the target device
     model.to(device)
-    
+
     # 5. Turn on model evaluation mode and inference mode
     model.eval()
     with torch.inference_mode():
         # Add an extra dimension to the image
         target_image = target_image.unsqueeze(dim=0)
-    
+
         # Make a prediction on image with an extra dimension and send it to the target device
         target_image_pred = model(target_image.to(device))
-        
+
     # 6. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
     target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
 
     # 7. Convert prediction probabilities -> prediction labels
     target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
-    
+
     # 8. Plot the image alongside the prediction and prediction probability
-    plt.imshow(target_image.squeeze().permute(1, 2, 0)) # make sure it's the right size for matplotlib
+    plt.imshow(target_image.squeeze().permute(1, 2, 0))  # make sure it's the right size for matplotlib
     if class_names:
         title = f"Pred: {class_names[target_image_pred_label.cpu()]} | Prob: {target_image_pred_probs.max().cpu():.3f}"
         print(image_path + ': ' + title)
-    else: 
+    else:
         title = f"Pred: {target_image_pred_label} | Prob: {target_image_pred_probs.max().cpu():.3f}"
         print(title)
